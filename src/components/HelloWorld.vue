ec963706f10926604c69130d6b92f0d4e86644b0
Saurabh Gupta <saurabhgupta0508@gmail.com>
2018-10-30 13:49:56 +0530

    Added Project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,108 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="autocomplete">
+      <input type="text" v-model="search" @input="onChange" @focus="showOptions"/>
+      <button type="button" @click="clearSearch"> clear</button>
+      <ul class="autocomplete-results" v-show="isOpen">
+        <li class="autocomplete-result" @focus="showOptions" @blur="hideOptions" v-for="repos in results" :key="repos.id" @click="showResults(repos)">
+          {{repos.full_name}}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <p> <a :href="selected.html_url"> {{selected.full_name }} </a> </p>
+      <p>{{selected.description}}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      search: "",
+      results: [],
+      loading: false,
+      isOpen: false,
+      selected: {}
+    };
+  },
+  methods: {
+    onChange: _.debounce(function() {
+      this.selected = {};
+      this.loading = true;
+      this.isOpen = true;
+      if (this.search.length > 3) {
+        //this.loading = true;
+        fetch(`https://api.github.com/search/repositories?q=${this.search}`)
+          .then(response => response.json())
+          .then(json => {
+            this.loading = false;
+            console.log(json);
+            this.results = json.items;
+          });
+      } else {
+        (this.results = []), (this.isOpen = false);
+        this.loading = false;
+      }
+    }, 300),
+    showResults(repo) {
+      this.isOpen = false;
+      this.selected = repo;
+    },
+    showOptions() {
+      if(this.search && this.results.length > 0) {
+        this.isOpen = true;
+      }
+    },
+    hideOptions() {
+      this.isOpen = false;
+    },
+    clearSearch() {
+      this.search = "";
+      this.results = [];
+      this.isOpen = false;
+    }
+  },
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+}
+
+.autocomplete {
+  position: relative;
+  width: 200px;
+}
+
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 300px;
+  width: auto;
+  overflow: auto;
+}
+
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+}
+
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
 }
 </style>
